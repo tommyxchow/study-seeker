e2843e87db27b76e51718fc286b4431ab2a1b16b
Jeffrey Xu <jkxu@buffalo.edu>
2022-03-08 16:46:28 -0500

    various ui elements of the landing page have been turned into react components

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import "./App.css";
 
-function Hello(){
+function BackgroundImage(props){
     return(
-        <div>hello! (this is just for experimenting)</div>
+        <div className="background-image">
+            {props.children}
+        </div>
     );
 }
+
+function LandingPageBox(props){
+    return(
+        <div className="landing-page-box">
+            {props.children}
+        </div>
+    );
+}
+
+function LogoText(){
+    return(
+        <span className="logo-text">
+        Study
+        <br />
+        Seeker
+        <br />
+      </span>
+    );
+}
+function LogoSubtext(){
+    return( <span className="logo-subtext">SEEK FRIENDS TO STUDY</span>);
+}
+function ButtonSubmitForm(props){
+    return(
+        <form action= {props.link} >
+        <button className="buttonSubmitForm">{props.buttonText}</button>
+      </form>
+    );
+}
+/*
+function ButtonSubmitForm({link, buttonText}){
+    return(
+        <form action={link}>
+        <button className="buttonSubmitForm">{buttonText}</button>
+      </form>
+    );
+}
+*/
 
 class LandingPage extends React.Component {
 
@@ -15,32 +55,23 @@ class LandingPage extends React.Component {
     render () {
         return (
             <>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" type="text/css" href="styleStudy.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@1,600&family=Sofia&display=swap"
-    rel="stylesheet"
-  />
-  <div className="background-image">
-    <div className="landing-page-box">
-      <span className="logo-text">
-        Study
-        <br />
-        Seeker
-        <br />
-      </span>
-      <span className="logo-subtext">SEEK FRIENDS TO STUDY</span>
-      <form action="/register/">
-        <button className="buttonSubmitForm">Sign Up</button>
-      </form>
-      <form action="https://www.google.com/">
-        <button className="buttonSubmitForm">Login</button>
-      </form>
-    </div>
-  </div>
-</>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="stylesheet" type="text/css" href="styleStudy.css" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+            <link
+            href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@1,600&family=Sofia&display=swap"
+            rel="stylesheet"/>
+
+            <BackgroundImage>
+                <LandingPageBox>
+                    <LogoText/>
+                    <LogoSubtext/>
+                    <ButtonSubmitForm link ={"/register/"} buttonText={"Sign Up"}/>
+                    <ButtonSubmitForm link ={"https://www.google.com/"} buttonText={"Login"} />
+                </LandingPageBox>
+            </BackgroundImage>
+            </>
         );
 
     }
