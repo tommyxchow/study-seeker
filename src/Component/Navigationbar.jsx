be331ee549ca888079b015af9e5690abbeae607b
tommyxchow <tommyxchow@gmail.com>
2022-03-10 15:00:55 -0500

    Fix navbar overlapping pages (#54)

--- src/Component/Navigationbar.jsx
+++ src/Component/Navigationbar.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import "../App.css";
-import navbar from "./nav.module.css";
-import {
-   Link
-} from 'react-router-dom';
+import styles from "./nav.module.css";
 
-/* The Navbar class provides navigation through react router links.  Note the callback
-   to the parent app class in the last entry... this is an example of calling a function
+/* The Navbar class provides navigation through react router anks.  Note the callback
+   to the parent app class in the last entry... this is an example of calang a function
    passed in via props from a parent component */
 class Navigationbar extends React.Component {
-
   render() {
     return (
-        <div className="sidenav">                      
-        <ul id="side-menu-items">
-            <li><h className={navbar.home}>Home</h></li>
-            <li><h className={navbar.search}>Search</h></li>
-            <li><h className={navbar.profile}>Profile</h></li>
-            <li><h className={navbar.groups}>Groups</h></li>
-            <li><h className={navbar.connections}>Connections</h></li>
-            <li><h className={navbar.logouttext}>Logout</h></li>
-            <li><h className={navbar.logoutarrow}></h></li>
-            <li><h className={navbar.logoutbackground}></h></li>
-        </ul>
-     </div>
-  );
+      <div className={styles.sidenav}>
+        <a href="/" className={styles.home}>
+          Home
+        </a>
+        <a href="/" className={styles.search}>
+          Search
+        </a>
+        <a href="/settings" className={styles.profile}>
+          Profile
+        </a>
+        <a href="/" className={styles.groups}>
+          Groups
+        </a>
+        <a href="/connections" className={styles.connections}>
+          Connections
+        </a>
+        <div className={styles.spacer}></div>
+        <a href="/" className={styles.logouttext}>
+          Logout
+        </a>
+      </div>
+    );
   }
-
 }
+
 export default Navigationbar;
