18e0f0f3989d90b1d3de29b8d1e6b2f28a1323b4
jiyahou <a@DESKTOP-ED31OR7.localdomain>
2022-03-17 16:59:18 -0400

    edit connectionIDs and userID handling #55

--- src/Component/ConnectionRequest.jsx
+++ src/Component/ConnectionRequest.jsx
@@ -1,16 +1,9 @@
 import React, { useState } from "react";
 import "../App.css";
 
-import personIcon from "../assets/Ibn_Khaldun.png";
 import litStarIcon from "../assets/litstar.svg";
 import starIcon from "../assets/unlitstar.svg";
-import personIcon2 from "../assets/zhuangzi.svg";
 import connections from "./connections.module.css";
-import lisaMoan from "../assets/lisamoan.png";
-import jayHou from "../assets/jayhou.png";
-import monaLia from "../assets/monalia.png";
-import tommypic from "../assets/tommy.png";
-
 
 var myHeaders = new Headers();
 myHeaders.append("accept", "*/*");
@@ -19,6 +12,25 @@ myHeaders.append("Authorization", "Bearer " + sessionStorage.getItem("token"))
 
 var requestOptionsGet = {
   method: 'GET',
+  headers: myHeaders,
+};
+
+var raw = JSON.stringify({
+  "fromUserID": 10,
+  "toUserID": 2,
+  "attributes": {
+    "status": "accepted"
+  }
+});
+
+var requestOptionsPatch = {
+  method: 'PATCH',
+  headers: myHeaders,
+  body: raw,
+};
+
+var requestOptionsDelete = {
+  method: 'DELETE',
   headers: myHeaders,
 };
 
@@ -31,7 +43,7 @@ export default class ConnectionRequest extends React.Component{
       responseMessage: "",                                      //List of accepted friends( used for my connection list )
       connection_id: -1,
       connection_id_list: [],
-      fromUserID: [],
+      fromUserID: "",
       profile: this.props.userid,
   }; 
   this.fieldChangeHandler.bind(this);
@@ -51,6 +63,8 @@ export default class ConnectionRequest extends React.Component{
 
   removeHandler_Accept(givenName,givenID){                        //handle and remove connection after being accepted
     console.log('this is: ', givenName);
+    console.log('userid: ', this.state.fromUserID);
+    console.log('connectionid: ', this.state.connection_id);
     const newList=this.state.names.filter(name => name[0] !== givenID);
     const newConnectionList=this.state.connection_id_list.filter(id => id[0] !== this.state.connection_id);
     this.setState(prevState => ({
@@ -58,7 +72,10 @@ export default class ConnectionRequest extends React.Component{
       }))
     this.setState({names: newList});
     this.setState({connection_id: newConnectionList[0]});
-    console.log(givenName[1]);
+    // fetch("https://webdev.cse.buffalo.edu/hci/api/api/commitment/connections/" + this.state.connection_id, requestOptionsPatch)
+    //   .then(response => response.text())
+    //   .then(result => console.log(result))
+    //   .catch(error => console.log('error', error));
     this.forceUpdate();
     console.log('added: ', this.state.accepted_friends);
     console.log('connection-id:', this.state.connection_id);    
@@ -68,10 +85,16 @@ export default class ConnectionRequest extends React.Component{
 
   removeHandler_Reject(givenName,givenID){                        //handle and remove connection after being rejected
     console.log('this is: ', givenName);
+    console.log('userid: ', this.state.fromUserID);
+    console.log('connectionid: ', this.state.connection_id);
     const newList=this.state.names.filter(name => name[0] !== givenID);
     const newConnectionList=this.state.connection_id_list.filter(id => id[0] !== this.state.connection_id);
     this.setState({names: newList});
     this.setState({connection_id: newConnectionList[0]});
+    // fetch("https://webdev.cse.buffalo.edu/hci/api/api/commitment/connections/" + this.state.connection_id, requestOptionsDelete)
+    //   .then(response => response.text())
+    //   .then(result => console.log(result))
+    //   .catch(error => console.log('error', error));
     this.forceUpdate();
     console.log('added: ', this.state.accepted_friends);
     console.log('names: ', this.state.names);
@@ -99,18 +122,20 @@ export default class ConnectionRequest extends React.Component{
           arrUserID.push(json[a].fromUserID);
           connectionToID.push([json[a].id,json[a].fromUserID]);
         }
-        // console.log("arrConnections", arrConnections);
-        // console.log("arrUserID" ,arrUserID);
-        //console.log("arrUserID", arrUserID);
-        this.setState({fromUserID: this.state.names.concat(arrUserID)});
+        console.log("arrConnections", arrConnections[0]);
+        console.log("arrUserID" ,arrUserID[0]);
+        // this.setState({fromUserID: this.state.names.concat(arrUserID)});
         this.setState({connection_id_list: this.state.connection_id_list.concat(arrConnections)});
         for(var b = 0; b < arrConnections.length; b++){
+          this.setState({connection_id: arrConnections[b]});
+          this.setState({fromUserID: arrUserID[b]});
           fetch("https://webdev.cse.buffalo.edu/hci/api/api/commitment/connections/" + arrConnections[b], requestOptionsGet)
           .then(response => response.json())
           .then(result => {
             console.log(result);
             this.setState({names: this.state.names.concat(result.fromUser.attributes.firstName + " " + result.fromUser.attributes.lastName[0] + ".")});
-            this.setState({connection_id: result.id});
+            // this.setState({connection_id: result.id});
+            // this.setState({fromUserID: result.fromUserID});
           })
           .catch(error => console.log('error', error));
         }
@@ -120,8 +145,10 @@ export default class ConnectionRequest extends React.Component{
     // console.log("Testing", this.state);
     console.log("user:", sessionStorage.getItem("user"));
     console.log("arrConnections", arrConnections);
-    console.log("connection_id", this.state.connection_id_list);
+    console.log("connection_id_list", this.state.connection_id_list);
     console.log("connection to ID", connectionToID);
+    console.log("fromUserID", this.state.fromUserID);
+    console.log("connection_id", this.state.connection_id);
   }
 //test tommys image
   findUserPicture(){
@@ -135,7 +162,7 @@ export default class ConnectionRequest extends React.Component{
       console.log(path)
       return path;
   }
-
+// Mona L. id = 35, connectionID = 193, Lisa M. id = 34, connectionID = 194
   render() {
     // console.log("arrUserID", arrUserID);
     // this.setState({
@@ -190,8 +217,8 @@ export default class ConnectionRequest extends React.Component{
                           <img className={connections.picturecircle} img src={"https://webdev.cse.buffalo.edu/hci/api/uploads/files/DOo1Ebbt8dYT4-plb6G6NP5jIc9_l_gNlaYwPW4SaBM.png"} alt="default img"/> 
                           <div className={connections.name}>{name}</div>
                           <div className={connections.stars}>
-                              <img className={connections.star1} img src={litStarIcon} alt="star"/>
-                              <img className={connections.star2} img src={litStarIcon} alt="star"/>
+                              <img className={connections.star1} img src={starIcon} alt="star"/>
+                              <img className={connections.star2} img src={starIcon} alt="star"/>
                               <img className={connections.star3} img src={starIcon} alt="star"/>
                               <img className={connections.star4} img src={starIcon} alt="star"/>
                               <img className={connections.star5} img src={starIcon} alt="star"/>
