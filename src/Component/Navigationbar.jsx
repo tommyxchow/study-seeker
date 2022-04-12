5a924cfea89bbd7998fc68a06b0157bb9b729a93
Melisha Shakya <melishas@buffalo.edu>
2022-04-12 15:11:12 -0400

    put search path into navbar #95

--- src/Component/Navigationbar.jsx
+++ src/Component/Navigationbar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import styles from "./nav.module.css";
-import {Link} from "react-router-dom";
 
 /* The Navbar class provides navigation through react router anks.  Note the callback
    to the parent app class in the last entry... this is an example of calang a function
@@ -11,50 +10,45 @@ class Navigationbar extends React.Component {
   render() {
     return (
       <div className={styles.sidenav}>
-
-        <Link to="/home" className={styles.home}>
-          Home
-        </a>
-        <a href="./search" className={styles.search}>
-          Search
-
+      {/* <Link to= "/"> */}
         <a href="/" className={styles.home}>
           Home
         </a>
-        <div className={styles.between}/>
-        <a href= "/">
-          <div className={styles.search}>
-            Search
-          </div>
-        </a>
-        <div className={styles.between}/>
-        <a href="/profile">
-          <div className={styles.profile}>
-            Profile
-          </div>
-        </a>
-        <div className={styles.between}/>
-        <a href="/groups">
-          <div className={styles.groups}>
-            Groups
-          </div>
-        </a>
-        <div className={styles.between}/>
-        <a href="/connections">
-          <div className={styles.connections}>
-            Connections
-          </div>
-        </a>
-        <div className={styles.spacer}></div>
-        <a href="/login">
-          <div
-            onClick={this.props.logout}
-            className={styles.logouttext}
-          >
-            Logout
-          </div>
-        </a>
-      </div>
+      {/* </Link> */}
+      <div className={styles.between}/>
+      <Link to= "./search">
+        <div className={styles.search}>
+          Search
+        </div>
+      </Link>
+      <div className={styles.between}/>
+      <a href="/profile">
+        <div className={styles.profile}>
+          Profile
+        </div>
+      </a>
+      <div className={styles.between}/>
+      <Link to="/groups">
+        <div className={styles.groups}>
+          Groups
+        </div>
+      </Link>
+      <div className={styles.between}/>
+      <Link to="/connections">
+        <div className={styles.connections}>
+          Connections
+        </div>
+      </Link>
+      <div className={styles.spacer}></div>
+      <Link to="/login">
+        <div
+          onClick={this.props.logout}
+          className={styles.logouttext}
+        >
+          Logout
+        </div>
+      </Link>
+    </div>
     );
   }
 }
