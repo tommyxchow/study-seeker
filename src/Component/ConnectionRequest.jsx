4ab4abde3a9a8d7d8927e59f397b2c5eb02986ea
tommyxchow <tommyxchow@gmail.com>
2022-05-03 11:37:19 -0400

    Fix conflicts in connection requests (#177)

--- src/Component/ConnectionRequest.jsx
+++ src/Component/ConnectionRequest.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 
@@ -22,6 +22,7 @@ var requestOptionsDelete = {
 };
 
 var user_id_map = {};
+var user_image_map = {};
 
 export default class ConnectionRequest extends React.Component {
   constructor(props) {
@@ -35,26 +36,22 @@ export default class ConnectionRequest extends React.Component {
       userID_list: [],
       fromUserID: -1,
       profile: this.props.userid,
+      imageID: "",
     };
     this.fieldChangeHandler.bind(this);
   }
 
   componentDidMount() {
     this.fetchUserIDs();
-    this.findUserPicture();
   }
 
   fieldChangeHandler(field, e) {
-    console.log("field change");
     this.setState({
       [field]: e.target.value,
     });
   }
 
   removeHandler_Accept(givenName) {
-    console.log("this is: ", givenName);
-    console.log("userid: ", this.state.fromUserID);
-    console.log("connectionid: ", this.state.connection_id);
     const newList = this.state.names.filter((name) => name !== givenName);
     const newConnectionList = this.state.connection_id_list.filter(
       (id) => id !== this.state.connection_id
@@ -92,9 +89,6 @@ export default class ConnectionRequest extends React.Component {
   }
 
   removeHandler_Reject(givenName) {
-    console.log("this is: ", givenName);
-    console.log("userid: ", this.state.fromUserID);
-    console.log("connectionid: ", this.state.connection_id);
     const newList = this.state.names.filter((name) => name !== givenName);
     const newConnectionList = this.state.connection_id_list.filter(
       (id) => id !== this.state.connection_id
@@ -121,6 +115,7 @@ export default class ConnectionRequest extends React.Component {
     var arrConnections = [];
     var arrUserID = [];
     var connectionToID = [];
+    //fetch all requests sent to the user
     fetch(
       "https://webdev.cse.buffalo.edu/hci/api/api/commitment/connections/?toUserID=" +
         sessionStorage.getItem("user"),
@@ -128,15 +123,13 @@ export default class ConnectionRequest extends React.Component {
     )
       .then((response) => response.json())
       .then((result) => {
-        console.log("fetch", result);
         for (var i = 0; i < result.length; i++) {
           if (result[0][i] === undefined) {
             break;
-          } else {
+          } else if (result[0][i].attributes.status === "pending") {
             json.push(result[0][i]);
           }
         }
-        console.log(json.length);
         for (var a = 0; a < json.length; a++) {
           arrConnections.push(json[a].id);
           arrUserID.push(json[a].fromUserID);
@@ -147,55 +140,61 @@ export default class ConnectionRequest extends React.Component {
               json[a].fromUser.attributes.lastName[0] +
               "."
           ] = json[a].fromUserID;
+          user_image_map[
+            json[a].fromUser.attributes.firstName +
+              " " +
+              json[a].fromUser.attributes.lastName[0] +
+              "."
+          ] = json[a].fromUser.attributes.profilePicture;
         }
-        for (var b = 0; b < arrConnections.length; b++) {
-          fetch(
-            "https://webdev.cse.buffalo.edu/hci/api/api/commitment/connections/" +
-              arrConnections[b],
-            requestOptionsGet
-          )
-            .then((response) => response.json())
-            .then((result) => {
-              console.log("result__", result);
-              console.log("result.attributes.status", result.attributes.status);
-              if (result.attributes.status === "accepted") {
-                this.setState((prevState) => ({
-                  accepted_friends: [
-                    result.fromUser.attributes.firstName +
-                      " " +
-                      result.fromUser.attributes.lastName[0] +
-                      ".",
-                    ...prevState.accepted_friends,
-                  ],
-                }));
-              } else {
-                this.setState({
-                  names: this.state.names.concat(
-                    result.fromUser.attributes.firstName +
-                      " " +
-                      result.fromUser.attributes.lastName[0] +
-                      "."
-                  ),
-                });
-                this.setState({
-                  connection_id_list: this.state.connection_id_list.concat(
-                    result.id
-                  ),
-                });
-                this.setState({
-                  userID_list: this.state.userID_list.concat(result.fromUserID),
-                });
-                this.setState({ connection_id: result.id });
-                this.setState({ fromUserID: result.fromUserID });
-                console.log("names", this.state.names);
-                console.log(
-                  "connection_id_list",
-                  this.state.connection_id_list
-                );
-                console.log("fromUserID list", this.state.userID_list);
-              }
-            })
-            .catch((error) => console.log("error", error));
+
+        if (arrConnections.length !== 0) {
+          for (var b = 0; b < arrConnections.length; b++) {
+            fetch(
+              "https://webdev.cse.buffalo.edu/hci/api/api/commitment/connections/" +
+                arrConnections[b],
+              requestOptionsGet
+            )
+              .then((response) => response.json())
+              .then((result) => {
+                if (result.attributes.status === "accepted") {
+                  this.setState((prevState) => ({
+                    accepted_friends: [
+                      result.fromUser.attributes.firstName +
+                        " " +
+                        result.fromUser.attributes.lastName[0] +
+                        ".",
+                      ...prevState.accepted_friends,
+                    ],
+                  }));
+                } else {
+                  this.setState({
+                    names: this.state.names.concat(
+                      result.fromUser.attributes.firstName +
+                        " " +
+                        result.fromUser.attributes.lastName[0] +
+                        "."
+                    ),
+                  });
+                  this.setState({
+                    connection_id_list: this.state.connection_id_list.concat(
+                      result.id
+                    ),
+                  });
+                  this.setState({
+                    userID_list: this.state.userID_list.concat(
+                      result.fromUserID
+                    ),
+                  });
+                  this.setState({ connection_id: result.id });
+                  this.setState({ fromUserID: result.fromUserID });
+                  this.setState({
+                    imageID: result.fromUser.attributes.profilePicture,
+                  });
+                }
+              })
+              .catch((error) => console.log("error", error));
+          }
         }
       })
       .catch((error) => console.log("error", error));
@@ -207,8 +206,8 @@ export default class ConnectionRequest extends React.Component {
       .then((result) => {
         for (var i = 0; i < result[0].length; i++) {
           let name = "";
-          let connectionId,
-            id = -1;
+          let id = -1;
+          let image = "";
           if (result[0][i].attributes.status == null) {
             continue;
           }
@@ -221,23 +220,19 @@ export default class ConnectionRequest extends React.Component {
               " " +
               result[0][i].toUser.attributes.lastName[0] +
               ".";
-            console.log(
-              "fromuser",
-              result[0][i].toUser.attributes.firstName +
-                " " +
-                result[0][i].toUser.attributes.lastName[0] +
-                "."
-            );
-            console.log(name);
-            console.log(this.state.accepted_friends);
             id = result[0][i].toUser.id;
+            image = result[0][i].toUser.attributes.profilePicture;
+            this.setState({
+              imageID: result[0][i].toUser.attributes.profilePicture,
+            });
             if (!this.state.accepted_friends.includes(name)) {
-              console.log(this.state.accepted_friends.includes(name));
               this.setState((prevState) => ({
                 accepted_friends: [name, ...prevState.accepted_friends],
               }));
             }
             user_id_map[name] = id;
+            user_image_map[name] = image;
+            this.forceUpdate();
           } else if (
             result[0][i].attributes.status == "accepted" &&
             result[0][i].toUserID == this.props.userid
@@ -247,48 +242,27 @@ export default class ConnectionRequest extends React.Component {
               " " +
               result[0][i].fromUser.attributes.lastName[0] +
               ".";
-            console.log(
-              "touser",
-              result[0][i].fromUser.attributes.firstName +
-                " " +
-                result[0][i].fromUser.attributes.lastName[0] +
-                "."
-            );
-            console.log(name);
             id = result[0][i].fromUser.id;
-            console.log(this.state.accepted_friends);
+            image = result[0][i].fromUser.attributes.profilePicture;
+            this.setState({
+              imageID: result[0][i].fromUser.attributes.profilePicture,
+            });
             if (!this.state.accepted_friends.includes(name)) {
-              console.log(this.state.accepted_friends.includes(name));
               this.setState((prevState) => ({
                 accepted_friends: [name, ...prevState.accepted_friends],
               }));
             }
             user_id_map[name] = id;
+            user_image_map[name] = image;
+            this.forceUpdate();
           }
         }
       })
       .catch((error) => console.log("error", error));
   }
-  //test tommys image
-  findUserPicture() {
-    var path = "";
-    fetch(
-      "https://webdev.cse.buffalo.edu/hci/api/api/commitment/users/12",
-      requestOptionsGet
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        path = JSON.stringify(result.attributes.profilePicture);
-      })
-      .catch((error) => console.log("error", error));
-    console.log(path);
-    return path;
-  }
-  // Mona L. id = 35, connectionID = 193, Lisa M. id = 34, connectionID = 194
+
   render() {
     let accepted = Array.from(new Set(this.state.accepted_friends));
-    console.log(accepted);
-    console.log(user_id_map);
     return (
       <div className="App">
         <div className={connections.requests}>Connection Requests</div>
@@ -296,46 +270,46 @@ export default class ConnectionRequest extends React.Component {
         <div className={connections.topcontainer}>
           {this.state.names.map((name) => {
             return (
-              <div key={name}>
+              <div key={name} className={connections.testdiv}>
                 <div className={connections.topdiv}>
-                  <img
-                    className={connections.picturecircle}
-                    src={
-                      "https://webdev.cse.buffalo.edu/hci/api/uploads/files/DOo1Ebbt8dYT4-plb6G6NP5jIc9_l_gNlaYwPW4SaBM.png"
-                    }
-                    alt="default img"
-                  />
                   <Link
                     to={"/profile/" + user_id_map[name]}
-                    className={connections.name}
+                    className={connections.link}
                   >
-                    {name}
+                    <img
+                      className={connections.picturecircle}
+                      src={
+                        "https://webdev.cse.buffalo.edu/" + user_image_map[name]
+                      }
+                      alt={name + "'s profile picture"}
+                    />
+                    <div className={connections.name}>{name}</div>
                   </Link>
                   <div className={connections.stars}>
                     <img
                       className={connections.star1}
                       src={starIcon}
-                      alt="star"
+                      alt="star 1"
                     />
                     <img
                       className={connections.star2}
                       src={starIcon}
-                      alt="star"
+                      alt="star 2"
                     />
                     <img
                       className={connections.star3}
                       src={starIcon}
-                      alt="star"
+                      alt="star 3"
                     />
                     <img
                       className={connections.star4}
                       src={starIcon}
-                      alt="star"
+                      alt="star 4"
                     />
                     <img
                       className={connections.star5}
                       src={starIcon}
-                      alt="star"
+                      alt="star 5"
                     />
                   </div>
                   <button
@@ -355,7 +329,6 @@ export default class ConnectionRequest extends React.Component {
             );
           })}
         </div>
-        {/* {console.log("testing",this.state.accepted_friends)} */}
         <div className={connections.connections}>Connections</div>
         <div className={connections.line2}> </div>
         <div className={connections.container}>
@@ -363,44 +336,44 @@ export default class ConnectionRequest extends React.Component {
             return (
               <div key={name}>
                 <div className={connections.div}>
-                  <img
-                    className={connections.picturecircle}
-                    src={
-                      "https://webdev.cse.buffalo.edu/hci/api/uploads/files/DOo1Ebbt8dYT4-plb6G6NP5jIc9_l_gNlaYwPW4SaBM.png"
-                    }
-                    alt="default img"
-                  />
                   <Link
                     to={"/profile/" + user_id_map[name]}
-                    className={connections.name}
+                    className={connections.link}
                   >
-                    {name}
+                    <img
+                      className={connections.picturecircle}
+                      src={
+                        "https://webdev.cse.buffalo.edu/" + user_image_map[name]
+                      }
+                      alt={name + "'s profile picture"}
+                    />
+                    <div className={connections.name}>{name}</div>
                   </Link>
                   <div className={connections.stars}>
                     <img
                       className={connections.star1}
                       src={starIcon}
-                      alt="star"
+                      alt="star 1"
                     />
                     <img
                       className={connections.star2}
                       src={starIcon}
-                      alt="star"
+                      alt="star 2"
                     />
                     <img
                       className={connections.star3}
                       src={starIcon}
-                      alt="star"
+                      alt="star 3"
                     />
                     <img
                       className={connections.star4}
                       src={starIcon}
-                      alt="star"
+                      alt="star 4"
                     />
                     <img
                       className={connections.star5}
                       src={starIcon}
-                      alt="star"
+                      alt="star 5"
                     />
                   </div>
                 </div>
@@ -408,14 +381,6 @@ export default class ConnectionRequest extends React.Component {
             );
           })}
         </div>
-        <img
-          className={connections.picturecircle}
-          src={
-            "https://webdev.cse.buffalo.edu/hci/api/uploads/files/x9sRVcdg8C5vip7apFO5cuR1jus4w00_6oOtB2bFKqg.png"
-          }
-          alt="img of Lisa Moan"
-          hidden
-        />
       </div>
     );
   }
